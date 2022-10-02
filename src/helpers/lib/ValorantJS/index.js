6fcac9b7161db5346dd994c33f36d763661ec4bf
Ryan Alexander <38785445+ryanalexander@users.noreply.github.com>
2022-10-02 09:02:01 +0000

    feat: Move ValorantJS

---
+++ src/helpers/lib/ValorantJS/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  // Internal Game APIs
+  API: require("./api"),
+  LocalRiotClientAPI: require("./LocalRiotClientAPI"),
+  Maps: require("./maps"),
+  Regions: require("./regions"),
+  Tiers: require("./tiers"),
+
+};
